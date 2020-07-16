a15823cfaff0bd6c9d7966d073b096a422ca9b70
dasamith <60905198+dasamith@users.noreply.github.com>
2020-07-16 18:02:23 -0400

    Updates

---
+++ precache-manifest.3ee5032cf6a4763ebc1169bb4577cfc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554a615a2a133fd4125efd2b8bc78dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f6466110bede398e10",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9854206bb7c599ec57a6",
+    "url": "/static/js/2.17a50a4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.17a50a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f6466110bede398e10",
+    "url": "/static/js/main.e9b3dbd4.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
